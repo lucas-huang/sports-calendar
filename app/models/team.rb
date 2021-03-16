8b28b0c8732db2cc31e89345d5e07a78f64841a8
lucashuang@berkeley.edu <lucashuang@berkeley.edu>
2021-03-15 17:10:41 -0700

    added form

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
 	has_many :home_matches, class_name: 'Match', foreign_key: 'home_team_id'
   	has_many :away_matches, class_name: 'Match', foreign_key: 'away_team_id'
+  	validates_presence_of :name, :city
 end
