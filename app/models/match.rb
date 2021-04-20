51304ae0f73f98a5d5621a08c293989947780e1e
lucas-huang <huanglucas1@gmail.com>
2021-04-20 16:22:34 -0700

    fixes

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,8 @@
 class Match < ApplicationRecord
-	def start_time
-		self.time
-	end
 	belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
   	belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
   	validates_presence_of :home_team_id, :away_team_id, :time, :location
+	def start_time
+		self.time
+	end
 end
